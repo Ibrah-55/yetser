9a35524fb358711092e389ce2304eac11537dfba
Ibrah-55 <kariukiibrahim55@gmail.com>
2024-10-11 21:31:14 +0300

    footer icons and admin welcome page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,32 +1,35 @@
-import React from 'react'
-import Navbar from './components/navbar/Navbar'
-import Sidebar from './components/sidebar/Sidebar'
-import { Route, Routes } from 'react-router-dom';
-import Add from './pages/add/Add';
-import List from './pages/list/List';
-import Orders from './pages/orders/Orders';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/add/Add";
+import List from "./pages/list/List";
+import Orders from "./pages/orders/Orders";
+import Welcome from "./pages/welcome/welcome";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-const url =
-  " https://9b3f-2c0f-2a80-10d1-e600-00-1002.ngrok-free.app" ||
-  "http://localhost:4000";
+  const url =
+    "https://9b3f-2c0f-2a80-10d1-e600-00-1002.ngrok-free.app" ||
+    "http://localhost:4000";
+
   return (
     <div>
-      <ToastContainer/>
-      <Navbar/>
+      <ToastContainer />
+      <Navbar />
       <hr />
       <div className="app-content">
-        <Sidebar/>
+        <Sidebar />
         <Routes>
-          <Route path='/add' element={<Add url={url} />} />
-          <Route path='/list' element={<List url={url} />} />
-          <Route path='/orders' element={<Orders url={url} />} />
+          <Route path="/" element={<Welcome />} /> {/* Add the welcome route */}
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
         </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
