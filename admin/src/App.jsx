954eef033e69aa2d6704b5fd5edb34a1d03792c0
Ibrah-55 <kariukiibrahim55@gmail.com>
2024-10-18 23:04:16 +0300

    Ngrok url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,7 +6,6 @@ import Add from "./pages/add/Add";
 import List from "./pages/list/List";
 import Orders from "./pages/orders/Orders";
 import Welcome from "./pages/welcome/welcome";
-import Edit from "./pages/edit/Edit"; // Import the Edit component
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -27,7 +26,7 @@ const App = () => {
           <Route path="/add" element={<Add url={url} />} />
           <Route path="/list" element={<List url={url} />} />
           <Route path="/orders" element={<Orders url={url} />} />
-          <Route path="/edit/:id" element={<Edit url={url} />} />{" "}
+          {/* <Route path="/edit/:id" element={<Edit url={url} />} />{" "} */}
           {/* Add the edit route */}
         </Routes>
       </div>
